0e4d73b4ea3f65d7d37c9a4a3c665bdddd00bc1c
Moontaha Rawshan <rawshanmoontaha@gmail.com>
2025-03-24 16:58:21 +0600

    Update urls.py

--- gitlms/lms/urls.py
+++ gitlms/lms/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     
-    path('department=<int:id>/', deptcourses, name='deptcourses')
-    
+    path('department=<int:id>/', deptcourses, name='deptcourses'),
+    path('department=<int:dept_id>/course=<int:course_id>', course_facs, name='course_facs')
 ]
