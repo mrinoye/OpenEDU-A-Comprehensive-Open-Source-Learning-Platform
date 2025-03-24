f3c5827ac39eb698a1ed69e8e7a1cb47aae99128
Moontaha Rawshan <rawshanmoontaha@gmail.com>
2025-03-24 16:58:24 +0600

    Update views.py

--- gitlms/lms/views.py
+++ gitlms/lms/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from .models import Department,Course
-
+from .models import Department,Course,Faculty
 # Create your views here.
 @login_required
 def deptcourses(request,id):
     department=Department.objects.get(id=id)
     courses = Course.objects.filter(department=department).order_by('course_name')
 
-    context={'name':request.user.username,'courses':courses}
+    context={'name':request.user.username,'courses':courses , 'department': department}
     return render(request,'deptcourses.html',context)
+
+@login_required
+def course_facs(request, dept_id, course_id):
+    department = Department.objects.get(id=dept_id)
+    course = Course.objects.get(id=course_id)
+    faculties = Faculty.objects.filter(course=course)
+    context = {'department': department, 'faculties': faculties}
+    return render(request, 'faculty.html', context)
