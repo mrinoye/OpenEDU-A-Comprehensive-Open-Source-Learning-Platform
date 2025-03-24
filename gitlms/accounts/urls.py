8357cc6fdfcd008f7991b2d3cdeb7f8f6c4c349e
abirmahmudrifat002 <abirmahmud.rifat002@gmail.com>
2025-03-24 17:00:50 +0600

    Created a welcome page

--- gitlms/accounts/urls.py
+++ gitlms/accounts/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', welcome, name='welcome'),
+    path('welcome/', welcome, name='welcome'),
     path('login/', login, name='login'),
     path('signup/', signup, name='signup'),
     path('logout/', logout_view, name='logout'),
