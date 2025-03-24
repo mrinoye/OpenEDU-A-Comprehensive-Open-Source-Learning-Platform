0c37b2abd7b792ec545c8df95f2500339256004d
Ezaz Asif <asifrafid8399@gmail.com>
2025-03-25 02:18:03 +0600

    Linked the lecture page with backend and made some tweaks

--- gitlms/lms/urls.py
+++ gitlms/lms/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     
     path('department=<int:id>/', deptcourses, name='deptcourses'),
-    path('department=<int:dept_id>/course=<int:course_id>', course_facs, name='course_facs')
+    path('department=<int:dept_id>/course=<int:course_id>', course_facs, name='course_facs'),
+    path('department=<int:dept_id>/course=<int:course_id>/faculty=<int:fac_id>',fac_lecs, name='fac_lecs')
 ]
--- gitlms/lms/views.py
+++ gitlms/lms/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Department,Course,Faculty
+
+
+
 # Create your views here.
+
+#for Courses inside a Department
 @login_required
 def deptcourses(request,id):
     department=Department.objects.get(id=id)
@@ -11,11 +16,24 @@ def deptcourses(request,id):
     context={'name':request.user.username,'courses':courses , 'department': department}
     return render(request,'deptcourses.html',context)
 
+
+
+#for faculties inside a Course
 @login_required
 def course_facs(request, dept_id, course_id):
     department = Department.objects.get(id=dept_id)
     course = Course.objects.get(id=course_id)
     faculties = Faculty.objects.filter(course=course)
-    print(faculties)
     context = {'department': department, 'faculties': faculties,'course':course}
     return render(request, 'faculty.html', context)
+
+
+
+#for lectures inside a Faculty
+@login_required
+def fac_lecs(request, dept_id, course_id,fac_id):
+    department = Department.objects.get(id=dept_id)
+    course = Course.objects.get(id=course_id)
+    faculty = Faculty.objects.get(id=fac_id)
+    context = {'department': department, 'faculty': faculty,'course':course}
+    return render(request, 'lectures.html', context)
