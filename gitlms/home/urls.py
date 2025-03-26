ad6b2442ad2513bd088c624625403b817bd2e2b9
Ezaz Asif <asifrafid8399@gmail.com>
2025-03-27 04:27:55 +0600

    Organized the codes further

--- gitlms/home/urls.py
+++ gitlms/home/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [
-    path('', home, name='home'),
+urlpatterns = []
+
+pages=[path('', home, name='home'),
     path('courses', courses, name='courses'),
     path('students', students, name='students'),
-    path('appoint', appoint, name='appoint'),
-    path('changerole/<int:userid>/<str:role>/', changerole, name='changerole')
-    
-]
+    path('appoint', appoint, name='appoint'),]
+
+functionalities=[ path('changerole/<int:userid>/<str:role>/', changerole, name='changerole')]
+
+urlpatterns+=pages
+urlpatterns+=functionalities
--- gitlms/lms/urls.py
+++ gitlms/lms/urls.py
@@ -1,15 +1,24 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [
+urlpatterns = []
+
+navigatelms=[
     path('', departments, name='departments'),
     path('department=<int:id>/', deptcourses, name='deptcourses'),
     path('department=<int:dept_id>/course=<int:course_id>', course_facs, name='course_facs'),
     path('department=<int:dept_id>/course=<int:course_id>/faculty=<int:fac_id>',fac_lecs, name='fac_lecs'),
     path('department=<int:dept_id>/course=<int:course_id>/faculty=<int:fac_id>/Lectures',lec_slides, name='lec_slides'),
-    path('department=<int:dept_id>/course=<int:course_id>/faculty=<int:fac_id>/Lectures/slide=<int:slide_id>',show_pdf, name='show_pdf'),
     path('department=<int:dept_id>/course=<int:course_id>/faculty=<int:fac_id>/Lectures/videos', lec_videos, name='lec_videos'),
-    path('department=<int:dept_id>/course=<int:course_id>/faculty=<int:fac_id>/Lectures/videos/video=<int:video_id>', show_video, name='show_video'),
     path('department=<int:dept_id>/course=<int:course_id>/faculty=<int:fac_id>/Lectures/notes', lec_notes, name='lec_notes'),
-    path('department=<int:dept_id>/course=<int:course_id>/faculty=<int:fac_id>/Lectures/notes/note=<int:note_id>', show_note, name='show_note')
 ]
+
+showcontents=[
+path('department=<int:dept_id>/course=<int:course_id>/faculty=<int:fac_id>/Lectures/slide=<int:slide_id>',show_pdf, name='show_pdf'),
+path('department=<int:dept_id>/course=<int:course_id>/faculty=<int:fac_id>/Lectures/videos/video=<int:video_id>', show_video, name='show_video'),
+path('department=<int:dept_id>/course=<int:course_id>/faculty=<int:fac_id>/Lectures/notes/note=<int:note_id>', show_note, name='show_note')
+]
+
+
+urlpatterns+=navigatelms
+urlpatterns+=showcontents
