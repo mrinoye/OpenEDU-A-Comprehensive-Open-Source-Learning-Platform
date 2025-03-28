d0b94679fe886e0d7f47a0fb67585b252dd65e46
Ezaz Asif <asifrafid8399@gmail.com>
2025-03-29 03:36:29 +0600

    updated notification field

--- gitlms/notifications/models.py
+++ gitlms/notifications/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from accounts.models import User
 
 class Notification(models.Model):
-    reciever = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
-    not_id=models.IntegerField()
+    recievers = models.ManyToManyField(User, related_name="notifications")
     message = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_notifications")
     type=models.CharField(max_length=6)
+    content_id= models.IntegerField(default=0, null=True, blank=True)
+    real_content_id=models.IntegerField(default=0, null=True, blank=True)
+    type=models.CharField(max_length=10)
+    content_type=models.CharField(max_length=15)
     def __str__(self):
-        return f"Notification for {self.sender.username}: {self.message}"
+        return f"Notification from {self.sender.username}: {self.message}"
