84efe7be1d7af9326af816026be5dc29eb6eeaa5
Moontaha Rawshan <rawshanmoontaha@gmail.com>
2025-04-18 23:01:08 +0600

    updated the content factory to incorporate the adapter

--- gitlms/lms/contentFactory.py
+++ gitlms/lms/contentFactory.py
@@ -1,136 +1,102 @@
 from .models import Slide, Video, Note, temp_Slide, temp_Video, temp_Note
-from .contentUploadStratagy import SlideUploadStrategy, VideoUploadStrategy, NoteUploadStrategy, TempSlideUploadStrategy, TempVideoUploadStrategy, TempNoteUploadStrategy
-
-
+from .contentUploadAdapter import ContentUploadAdapter
 from abc import ABC, abstractmethod
 
+
 class AbstractContentFactory(ABC):
-    
     @abstractmethod
     def create_content(self, faculty, name, content_file):
         pass
 
 
 class SlideFactory(AbstractContentFactory):
-    
-    def create_content( faculty, name, content_file):
-        # Create the Slide model instance
+    def create_content(self, faculty, name, content_file):
         slide_instance = Slide(
             name=name,
             faculty=faculty,
             content=content_file,
         )
-
-        # Set the upload strategy
-        upload_strategy = SlideUploadStrategy()
+        adapter = ContentUploadAdapter(content_type='slide', is_temp=False)
+        upload_strategy = adapter.get_upload_strategy()
         slide_instance.content.upload_to = upload_strategy.get_upload_to
-
-        # Save the Slide instance
         slide_instance.save()
         return slide_instance
 
 
 class VideoFactory(AbstractContentFactory):
-    
-    def create_content( faculty, name, content_file):
-        # Create the Video model instance
+    def create_content(self, faculty, name, content_file):
         video_instance = Video(
             name=name,
             faculty=faculty,
             content=content_file,
         )
-
-        # Set the upload strategy
-        upload_strategy = VideoUploadStrategy()
+        adapter = ContentUploadAdapter(content_type='video', is_temp=False)
+        upload_strategy = adapter.get_upload_strategy()
         video_instance.content.upload_to = upload_strategy.get_upload_to
-
-        # Save the Video instance
         video_instance.save()
         return video_instance
 
 
 class NoteFactory(AbstractContentFactory):
-    
-    def create_content(faculty, name, content_file):
-        # Create the Note model instance
+    def create_content(self, faculty, name, content_file):
         note_instance = Note(
             name=name,
             faculty=faculty,
             content=content_file,
         )
-
-        # Set the upload strategy
-        upload_strategy = NoteUploadStrategy()
+        adapter = ContentUploadAdapter(content_type='note', is_temp=False)
+        upload_strategy = adapter.get_upload_strategy()
         note_instance.content.upload_to = upload_strategy.get_upload_to
-
-        # Save the Note instance
         note_instance.save()
         return note_instance
 
 
-
 class AbstractTemporaryContentFactory(ABC):
-
     @abstractmethod
-    def create_temp_content( real_instance, faculty, name, content_file):
+    def create_temp_content(self, real_instance, faculty, name, content_file):
         pass
 
 
 class TemporarySlideFactory(AbstractTemporaryContentFactory):
-    
-    def create_temp_content( real_instance, faculty, name, content_file):
-        # Create the temp_Slide model instance
+    def create_temp_content(self, real_instance, faculty, name, content_file):
         temp_slide_instance = temp_Slide(
             name=name,
-            real=real_instance,  # Link to the real Slide instance
+            real=real_instance,
             faculty=faculty,
             content=content_file,
         )
-
-        # Set the upload strategy
-        upload_strategy = TempSlideUploadStrategy()
+        adapter = ContentUploadAdapter(content_type='slide', is_temp=True)
+        upload_strategy = adapter.get_upload_strategy()
         temp_slide_instance.content.upload_to = upload_strategy.get_upload_to
-
-        # Save the temp_Slide instance
         temp_slide_instance.save()
         return temp_slide_instance
 
 
 class TemporaryVideoFactory(AbstractTemporaryContentFactory):
-    
-    def create_temp_content( real_instance, faculty, name, content_file):
-        # Create the temp_Video model instance
+    def create_temp_content(self, real_instance, faculty, name, content_file):
         temp_video_instance = temp_Video(
             name=name,
-            real=real_instance,  # Link to the real Video instance
+            real=real_instance,
             faculty=faculty,
             content=content_file,
         )
-
-        # Set the upload strategy
-        upload_strategy = TempVideoUploadStrategy()
+        adapter = ContentUploadAdapter(content_type='video', is_temp=True)
+        upload_strategy = adapter.get_upload_strategy()
         temp_video_instance.content.upload_to = upload_strategy.get_upload_to
-
-        # Save the temp_Video instance
         temp_video_instance.save()
         return temp_video_instance
 
 
 class TemporaryNoteFactory(AbstractTemporaryContentFactory):
-    
-    def create_temp_content( real_instance, faculty, name, content_file):
-        # Create the temp_Note model instance
+    def create_temp_content(self, real_instance, faculty, name, content_file):
         temp_note_instance = temp_Note(
             name=name,
-            real=real_instance,  # Link to the real Note instance
+            real=real_instance,
             faculty=faculty,
             content=content_file,
         )
-
-        # Set the upload strategy
-        upload_strategy = TempNoteUploadStrategy()
+        adapter = ContentUploadAdapter(content_type='note', is_temp=True)
+        upload_strategy = adapter.get_upload_strategy()
         temp_note_instance.content.upload_to = upload_strategy.get_upload_to
-
-        # Save the temp_Note instance
         temp_note_instance.save()
         return temp_note_instance
